c6891d6653aed377dfc54e869736acce412ea8bc
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2023-06-05 23:41:10 +0100

    update

--- angular-8-client/src/app/services/tutorial.service.ts
+++ angular-8-client/src/app/services/tutorial.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://129.151.237.68:8080//api/tutorials';
 
 @Injectable({
   providedIn: 'root'
